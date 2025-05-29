c672e183388c4cae947c2c2210e9408d9c7a4d10
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-05-29 16:53:02 +0200

    close tooltip on click outside
    this is usual behavior for toggletips

--- article-preview/script.js
+++ article-preview/script.js
@@ -9,6 +9,16 @@ const toggleTooltip = () => {
   }
 }
 
+closeOnClickOutside = (e) => {
+  const tt = qs(".tooltip")[0];
+  const inside = [tt, ...qs(".tt-trigger")].some(el => el.contains(e.target))
+  if (!inside) {
+    tt.style.display = "none"
+  }
+}
+
+document.addEventListener("click", closeOnClickOutside)
+
 qs(".tt-trigger").forEach(x => {
   x.addEventListener("click", toggleTooltip);
 })
