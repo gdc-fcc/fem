6be0156769489fa0fa6c998d8cbd63db631e6449
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-02 01:19:16 +0200

    code style: harmonize indents and semicolons

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,29 +1,29 @@
-const qs = q => document.querySelector(q)
+const qs = q => document.querySelector(q);
 
 document.querySelectorAll('[rel="preload"]').forEach(el => {
     el.rel = "stylesheet";
     el.removeAttribute("as");
 })
 
-const custom = document.querySelector(".option-custom")
+const custom = document.querySelector(".option-custom");
 const tipButtons = document.querySelectorAll(".tip-percent-options > div");
 
 tipButtons.forEach(el => {
     el.addEventListener("click", e => {
         const value = e.target.dataset.value;
-        setPercentage(value)
-        setBtnActive(value)
-        custom.value = ""
+        setPercentage(value);
+        setBtnActive(value);
+        custom.value = "";
     })
 })
 
 custom.addEventListener("input", e => {
-    setBtnActive("")
-    setPercentage(e.target.value)
+    setBtnActive("");
+    setPercentage(e.target.value);
 })
 
 qs(".bill").addEventListener("input", e => {
-    setState("bill", e.target.value)
+    setState("bill", e.target.value);
 })
 
 qs(".npeople").addEventListener("input", e => {
@@ -40,7 +40,7 @@ const isValid = () =>
 const renderState = () => {
     const {percentage, bill, people} = state;
     const total = bill * (1 + percentage/100);
-    const tip = bill * percentage/100
+    const tip = bill * percentage/100;
     qs("#tip").innerText = "$" + (tip/people).toFixed(2);
     qs("#total").innerText = "$" + (total/people).toFixed(2);
     qs("#reset").removeAttribute("disabled");
@@ -62,7 +62,7 @@ const setPercentage = (x) => {
 
 const setBtnActive = (val) => {
     tipButtons.forEach(el => {
-        el.dataset.active = el.dataset.value == val
+        el.dataset.active = el.dataset.value == val;
     })
 }
 
@@ -70,10 +70,10 @@ qs("button").addEventListener("click", e => {
     state.percentage = 0;
     state.bill = 0;
     state.people = 1;
-    setBtnActive("")
-    custom.value = ""
-    qs(".bill").value = ""
-    qs(".npeople").value = ""
+    setBtnActive("");
+    custom.value = "";
+    qs(".bill").value = "";
+    qs(".npeople").value = "";
     qs("#tip").innerText = "$0.00";
     qs("#total").innerText = "$0.00";
     qs("#reset").setAttribute("disabled", "true");
